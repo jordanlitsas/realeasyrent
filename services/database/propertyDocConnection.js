4a2066e42dd40a289249b81ec3fb682fe5b69cfe
Nathan-Cahill <87508548+Nathan-Cahill@users.noreply.github.com>
2021-09-17 17:38:16 +1000

    update connection files

--- services/database/propertyDocConnection.js
+++ services/database/propertyDocConnection.js
@@ -1,18 +1,125 @@
-const insertProperty = (property) => {
-    return true;
+var Propertydb = require();
+
+// create and save new user
+exports.create = (req,res)=>{
+    // validate request
+    if(!req.body){
+        res.status(400).send({ message : "Content can not be empty!"});
+        return;
+    }
+
+    // new property listing
+    const property = new propertydb({
+        property_ID : req.body.property_ID,
+        applicantCriteria : req.body.applicantCriteria,
+        Availabledate: req.body.Availabledate,
+        Bathrooms: req.body.Bathrooms,
+        bedrooms: req.body.bedrooms,
+        bidding: req.body.bidding,
+        Commuteprofile: req.body.Commuteprofile,
+        demographics: req.body.demographics,
+        Energylevels: req.body.Energylevels,
+        heating_cooling : req.body.heating_cooling,
+        Housing_type: req.body.Housing_type,
+        Indoorfeatures: req.body.Indoorfeatures,
+        keywords: req.body.keywords,
+        location: req.body.location,
+        Marketvalue: req.body.Marketvalue,
+        nbn: req.body.nbn,
+        Outdoorfeatures: req.body.Outdoorfeatures,
+        parking: req.body.parking,
+        pet: req.body.pet,
+        postcode: req.body.postcode,
+        price: req.body.price,
+        rent_type: req.body.rent_type,
+    
+    })
+
+    // save property into the database
+    property
+        .save(property)
+        .then(data => {
+            res.send(data)
+            res.redirect(''); //NEED HELP FOR REDIRECT
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message : err.message || "Some error occurred while creating a create operation"
+            });
+        });
+
 }
 
-const deleteProperty = (propertyId) => {
-    return true;
+// retrieve and return all properties/ retrive and return a single property
+exports.find = (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        Propertydb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found property with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving property with id " + id})
+            })
+
+    }else{
+        Userdb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving property information" })
+            })
+    }
+
+    
 }
 
-const getProperty = (propertyAttribute) => {
-    return true;
+// Update a new idetified property by property_id
+exports.update = (req, res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({ message : "Data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Update property with ${id}. Maybe property not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({ message : "Error Update property information"})
+        })
 }
 
-const updateProperty = (property) => {
+// Delete a property with specified property id in the request
+exports.delete = (req, res)=>{
+    const id = req.params.id;
 
-    return true;
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message : "property was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: "Could not delete property with id=" + id
+            });
+        });
 }
-
-module.exports = {insertProperty, deleteProperty, getProperty, updateProperty}
--- services/database/userDocConnection.js
+++ services/database/userDocConnection.js
@@ -1,30 +1,108 @@
-const insertUser = (user) => {
-    database.findOne(user, function(err, result){
-        if (result == null){
-            user.save();
-            res.status(200);
-            res.send({newUser: user});
-        } 
-        else {
-            res.status(400);
-            res.send({result: 'used'})
-        }
-    });
-    return true;
+var Userdb = require('../model/model');
+
+// create and save new user
+exports.create = (req,res)=>{
+    // validate request
+    if(!req.body){
+        res.status(400).send({ message : "Content can not be empty!"});
+        return;
+    }
+
+    // new user
+    const user = new Userdb({
+        ID : req.body.ID,
+        firstname : req.body.firstname,
+        surname : req.body.surname,
+        email : req.body.email,
+        postcode: req.body.postcode,
+        profiletype : req.body.profiletype
+    })
+
+    // save user in the database
+    user
+        .save(user)
+        .then(data => {
+            //res.send(data)
+            res.redirect('/add-user');
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message : err.message || "Some error occurred while creating a create operation"
+            });
+        });
+
 }
 
-const deleteUser = (userId) => {
-    return true;
+// retrieve and return all users/ retrive and return a single user
+exports.find = (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        Userdb.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Erro retrieving user with id " + id})
+            })
+
+    }else{
+        Userdb.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }
+
+    
 }
 
-const getUser = (userData) => {
-    return true;
+// Update a new idetified user by user id
+exports.update = (req, res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({ message : "Data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Update user with ${id}. Maybe user not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({ message : "Error Update user information"})
+        })
 }
 
-const updateUser = (newUserState) => {
-    //get id from newUserState
-    //findOneAndUpdate({userId: id})
-    return true;
-}
+// Delete a user with specified user id in the request
+exports.delete = (req, res)=>{
+    const id = req.params.id;
 
-module.exports = {insertUser, deleteUser, getUser, updateUser}
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
+            }else{
+                res.send({
+                    message : "User was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message: "Could not delete User with id=" + id
+            });
+        });
+}
