6c11644d061ecfaeb17defb1a53f330a05e666dc
Nathan-Cahill <87508548+Nathan-Cahill@users.noreply.github.com>
2021-10-08 14:00:40 +1100

    added socket serverside

--- util/notificationSocket.js
+++ util/notificationSocket.js
@@ -1,8 +1,3 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-
 //toast notifications
 const Toast = {
   init(){
